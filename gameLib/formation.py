dca132614d22fb5be2451afcc7f72b2913ec97a6
kuhnn <kuhnnat000@gmail.com>
2023-02-11 11:34:05 -0800

    restructure enemy storage in formation

--- gameLib/formation.py
+++ gameLib/formation.py
@@ -6,8 +6,8 @@ from gameLib import *
 MOVE_SPEED = 0.07
 MOVE_PADDING = 20
 
-ADVANCE_MIN = 100
-ADVANCE_MAX = 1500
+ADVANCE_MIN = 1000
+ADVANCE_MAX = 3000
 
 class Formation(pygame.sprite.Group):
 
@@ -18,7 +18,8 @@ class Formation(pygame.sprite.Group):
         self._spacing = spacing + DEFAULT_SIZE
         self._world = world
 
-        self._ships = []
+        self._main_ships = []
+        self._assault_ships = []
 
         self._center_x = world.size[0] / 2
         self._center_y = world.size[1] / 4
@@ -34,58 +35,56 @@ class Formation(pygame.sprite.Group):
         self._level_generator_instance = None
 
     def _square_level_generator(self):
-        self._ships = []
+        self._main_ships = []
+        self._assault_ships = []
 
         self._center_x = self._world.size[0] / 2
         self._center_y = self._world.size[1] / 4
 
+        advance_timer = pygame.time.get_ticks() + random.randint(ADVANCE_MIN, ADVANCE_MAX)
+        enemies_left = True
+
+        # Generate enemies in a square
         for i in range(self._size):
-            self._ships.append([])
             for j in range(self._size):
                 x = self._center_x + self._spacing * (j - self._size // 2)
                 y = self._center_y + self._spacing * (i - self._size // 2)
 
                 enemy = Enemy(self._world, Vector(x, y))
 
-                self._ships[i].append(enemy)
+                self._main_ships.append(enemy)
                 self.add(enemy)
 
-        fire_timer = pygame.time.get_ticks() + random.randint(0, 100)
-
-        enemies_left = True
+        # Update generator loop
         while enemies_left:
-
             self._center_x += self._move_x * self._world.delta_time
             if (self._move_x > 0 and self._center_x > self._max_x - random.randint(0, 100)):
-                self._move_x = 0
                 self._move_x = -MOVE_SPEED
 
             elif (self._move_x < 0 and self._center_x < self._min_x + random.randint(0, 100)):
-                self._move_x = 0
                 self._move_x = MOVE_SPEED
         
-            if (fire_timer < pygame.time.get_ticks()):
-                fire_timer = pygame.time.get_ticks() + random.randint(ADVANCE_MIN, ADVANCE_MAX)
-                random.choice(random.choice(self._ships)).fire()
+            if (advance_timer < pygame.time.get_ticks()):
+                advance_timer = pygame.time.get_ticks() + random.randint(ADVANCE_MIN, ADVANCE_MAX)
+                random.choice(self._main_ships).fire()
 
             enemies_left = False
-            for i in range(self._size):
-                for j in range(self._size):
-                    x = self._center_x + self._spacing * (j - self._size // 2)
-                    y = self._center_y + self._spacing * (i - self._size // 2)
+            temp_array = []
+            for i in range(len(self._main_ships)):
+                ship = self._main_ships[i]
+                ship.position.x = ship.position.x + self._move_x * self._world.delta_time
 
-                    self._ships[i][j].position.x = x
-                    self._ships[i][j].position.y = y
+                if ship.alive():
+                    enemies_left = True
+                    temp_array.append(ship)
 
-                    if self._ships[i][j].alive():
-                        enemies_left = True
+            self._main_ships = temp_array
 
             yield True
         
         yield False
 
     def update(self):
-        
         if self._level_generator_instance is None:
             self._level_generator_instance = self._square_level_generator()
         
