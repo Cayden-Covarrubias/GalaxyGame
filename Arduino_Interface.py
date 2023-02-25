fc596efd37509c80c50ccbed02deee1946f354e6
cayden <cjcovarr@calpoly.edu>
2023-02-24 18:23:27 -0800

    There was a sleep statement in arduino interface

--- Arduino_Interface.py
+++ Arduino_Interface.py
@@ -3,18 +3,19 @@ import time
 
 from gameLib import *
 
-arduino = serial.Serial(port='COM8', baudrate=9600, timeout=.1)
+#arduino = serial.Serial(port='COM8', baudrate=9600, timeout=.1)
 
 
-def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
-    time.sleep(0.05)
-    data = arduino.readline()
-    return data
+# def write_read(x):
+#     arduino.write(bytes(x, 'utf-8'))
+#     time.sleep(0.05)
+#     data = arduino.readline()
+#     return data
 
 
 def writescore(world):
-#num = 1
-    num = world.score
-    value = write_read(str(num))
-    print(value)
+    print (world.score)
+    #num = world.score
+   
+    #value = write_read(str(num))
+    #print(value)
